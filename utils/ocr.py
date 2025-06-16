0bdb157d65d07b0e54af5b5b9a3c7ad81961e108
Natshathra <natshathra04@gmail.com>
2025-06-16 22:18:42 +0530

    Create ocr.py

---
+++ utils/ocr.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import pytesseract
+
+def extract_text_from_image(img: Image.Image) -> str:
+    gray_img = img.convert('L')
+    return pytesseract.image_to_string(gray_img)
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
